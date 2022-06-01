a85f6c8218053a95fa70f031888cad7c9cde1643
SuperMario210 <Sup3rMar1o210@gmail.com>
2022-06-01 02:27:16 -0700

    added sql tables for paintings and categories

--- setup_pst.sql
+++ setup_pst.sql
@@ -15,3 +15,36 @@
 
 -- Is there a better way of keeping track of transactions so that seeing all of
 --  a token's transactions is fast?
+
+DROP TABLE IF EXISTS painting;
+DROP TABLE IF EXISTS category;
+
+CREATE TABLE category(
+    id      INT             PRIMARY KEY,
+    name    VARCHAR(63)     NOT NULL
+);
+
+INSERT INTO category VALUES
+	(1,	'Abstract'),
+	(2,	'Landscape'),
+	(3,	'Portrait'),
+	(4,	'Miscellaneous');
+
+CREATE TABLE painting(
+  id        INT             AUTO_INCREMENT,     -- 2345678
+  title     VARCHAR(63)     NOT NULL,           -- move.jpg
+  artist    VARCHAR(63)     NOT NULL,           -- Alvin On
+  price     NUMERIC(10,2)   NOT NULL,           -- 999.99
+  img_path  VARCHAR(127),                       -- imgs/move.png
+  category  INT, 
+  PRIMARY KEY (id),
+  FOREIGN KEY (category) REFERENCES category(id)
+);
+
+INSERT INTO painting(title, artist, price, img_path, category) VALUES
+	('Vigilant Cow', 'Mario Ruiz', 750.00, 'imgs/cow.png', 4),
+	('move.png', 'Alvin On', 250.00, 'imgs/move.png', 3),
+	('Smiling Face', 'Mario Ruiz', 250.00, 'imgs/smiling-face.png', 3),
+	('space-cat.png', 'Alvin On', 750.00, 'imgs/space-cat.png', 4),
+	('Useless Squiggle', 'Mario Ruiz', 100.00, 'imgs/squiggle.png', 1),
+	('Tree on a Hill', 'Mario Ruiz', 250.00, 'imgs/tree-on-a-hill.png', 2);
