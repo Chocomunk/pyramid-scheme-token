95b01708c5adcf626bf3a72f56db5161406ef1be
SuperMario210 <Sup3rMar1o210@gmail.com>
2022-05-31 23:27:31 -0700

    added starter web server code

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
