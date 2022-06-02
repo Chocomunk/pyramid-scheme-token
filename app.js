620b598436337df8a32fc35262db2e95ded90821
Chocomunk <alvin.zhou.on@gmail.com>
2022-06-02 07:27:54 -0700

    Add query filters

--- app.js
+++ app.js
@@ -3,6 +3,26 @@
 const express = require("express");
 const mysql = require("promise-mysql");
 
+const QUERY_STRING = `
+    SELECT painting.id, title, artist, price, img_path, name AS category 
+    FROM painting 
+    JOIN category ON painting.category = category.id
+`
+
+// TODO: Figure out how to non-magic-value this function
+function buildQuery(category, low, high) {
+    let query = QUERY_STRING;
+
+    // Add category filter
+    if (category)   query += `AND category.name = "${category}"`;
+
+    // Use an `array.join` to build the price filter
+    const prices = new Array();
+    if (low !== undefined)  prices.push(low);       // Allow '0'
+    if (high !== undefined) prices.push(high);
+    if (prices.length)  query += `WHERE ${prices.join(" AND ")}`
+}
+
 async function getDB() {
     let database = await mysql.createConnection({
         host: "localhost",
@@ -27,6 +47,18 @@ async function queryDB(query) {
 }
 
 const app = express();
+
+// TODO: error handling on category doesn't exist. Price out-of-range is fine.
+app.get("/api/pictures", async (req, res) => {
+    let query = buildQuery(req.query["category"], 
+                            req.query["low"], req.query["high"]);
+    try {
+        const rows = await queryDB(query);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
 app.get("/api/pictures/all", async (req, res) => {
     try {
