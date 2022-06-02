634cea5fbed78377980ec17561b1772bfc5cab63
SuperMario210 <Sup3rMar1o210@gmail.com>
2022-06-02 03:40:47 -0700

    added endpoints for querying pictures and categories

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 "use strict";
 
 const express = require("express");
+const mysql = require("promise-mysql");
+
+async function getDB() {
+    let database = await mysql.createConnection({
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "root",
+        database: "pst"
+    });
+    return database;
+}
+
+async function queryDB(query) {
+    let db;
+    try {
+        db = await getDB();
+        return await db.query(query);
+    } finally {
+        if (db) {
+            db.end();
+        }
+    }
+}
+
 const app = express();
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+app.get("/api/pictures/all", async (req, res) => {
+    try {
+        const rows = await queryDB(`
+            SELECT painting.id, title, artist, price, img_path, name AS category 
+            FROM painting 
+            JOIN category ON painting.category = category.id;
+        `);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
+
+app.get("/api/categories/", async (req, res) => {
+    try {
+        const rows = await queryDB("SELECT name FROM category;");
+        res.json(rows.map(row => row["name"]));
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
